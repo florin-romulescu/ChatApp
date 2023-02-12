0d2722facdc49698030cba8186f7d868c09a571a
Florin Romulescu <romulescu.florin.dev@gmail.com>
2023-02-12 14:42:08 +0200

    Contine structura de baza pentru rularea aplicatiei

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import redirect, render_template
+from flask import url_for
+
+app = Flask(__name__)
 
 @app.route("/")
 def init():
-    return "<h1>ChatApp</h1>"
+    return redirect("/index")
 
-if __name__ == '__main__':
-    app = Flask(__name__)
+@app.route("/index")
+def index():
+    return render_template("index.html")
