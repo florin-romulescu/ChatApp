c4eabe325033a94c1ce4b182fa22167ce48dc3f9
Florin Romulescu <romulescu.florin.dev@gmail.com>
2023-02-13 10:44:15 +0200

    Am adaugat url-urile de baza pentru paginile de register, login, root si about

--- views.py
+++ views.py
@@ -5,8 +5,16 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "Test"
+    return render_template("index.html")
 
 @views.route("/auth")
 def auth():
     return render_template("auth.html")
+
+@views.route("/register")
+def register():
+    return render_template("register.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
